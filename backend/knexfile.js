ecc155484230caed1e00d93979f5654378130492
cyrano007 <cyrano007@xemail.de>
2021-06-05 19:37:51 +0200

    finish

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -2,16 +2,16 @@ module.exports = {
     development: {
       client: 'pg',
       connection: process.env.DATABASE_URL || 'postgres://localhost/agility-dev',
-      pool: { min: 0, max: 7 }
+      pool: { min: 0, max: 1 }
     },
     stage: {
       client: 'pg',
       connection: process.env.DATABASE_URL || 'postgres://localhost/agility-dev',
-      pool: { min: 0, max: 7 }
+      pool: { min: 0, max: 1 }
     },
     production: {
       client: 'pg',
       connection: process.env.DATABASE_URL,
-      pool: { min: 0, max: 7 }
+      pool: { min: 0, max: 1 }
     }
   }
